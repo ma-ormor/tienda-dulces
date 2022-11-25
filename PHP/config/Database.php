9ed7f81edfcfa6d6ec18890865e96e726e2699eb
ma-ormor <113395368+ma-ormor@users.noreply.github.com>
2022-11-25 16:27:12 -0600

    Add files via upload

---
+++ PHP/config/Database.php
@@ -0,0 +1,30 @@
+<?php 
+  class Database {
+    // DB Params
+    private $host;
+    private $db_name = 'tienda_dulces';
+    private $username = 'root';
+    private $password;
+    private $conn;
+
+    function __construct(){
+      if(($host = getenv("HOST")) !== false) 
+        $host = 'localhost';
+      if(($password = getenv("DB_PASS")) !== false) 
+        $password = '';
+    }
+
+    // DB Connect
+    public function connect() {
+      $this->conn = null;
+
+      try { 
+        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      } catch(PDOException $e) {
+        echo 'Connection Error: ' . $e->getMessage();
+      }
+
+      return $this->conn;
+    }
+  }
